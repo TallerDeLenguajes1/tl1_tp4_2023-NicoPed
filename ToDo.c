1c27b63f92b3ce4130676c92c9c623b8e96ccbb0
slackku <bananuya3@gmail.com>
2023-04-13 18:42:06 -0300

    Punto1 y Punto 2 del ToDO herhos

--- ToDo.c
+++ ToDo.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX 100
+
 typedef struct Tarea
 {
     
@@ -13,8 +15,23 @@ typedef struct Tarea
 
 
 int main (){
+    char buffer[MAX];
+    int cantTareas;
+    printf ("\nCuantas tareas va a cargar?");
+    scanf("%d", &cantTareas);
 
+    sTAREA ** tareasPendientes;
+    sTAREA ** tareasRealizadas;
 
+    tareasPendientes = (sTAREA ** ) malloc(sizeof(sTAREA *) * cantTareas);
+    tareasRealizadas = (sTAREA ** ) malloc(sizeof(sTAREA *) * cantTareas);
 
+    for (int i = 0; i < cantTareas; i++)
+    {
+        //ES una buena practica hacer esto
+        tareasPendientes[i] = NULL;
+        tareasRealizadas[i] = NULL;
+    }
+    
     return 0;
 }
