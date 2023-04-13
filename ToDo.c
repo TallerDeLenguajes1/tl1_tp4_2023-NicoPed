4fdf1a565f756d5c85d50eafe851180650fa7a83
slackku <bananuya3@gmail.com>
2023-04-13 18:19:54 -0300

    Inicial Commit

---
+++ ToDo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Tarea
+{
+    
+    int TareaID;
+    char *Descripcion;
+    int Duracion;
+
+}sTAREA;
+
+
+int main (){
+
+
+
+    return 0;
+}
