41eaf9338f96da06ff0b01bda96fae356d175ce2
NicoPed <nikopedraza10@gmail.com>
2023-04-16 15:37:11 -0300

    Realizado las funciones de busqueda en lista-enlazada

--- ToDo.c
+++ ToDo.c
@@ -24,26 +24,29 @@ sNodo* crearNodo(int id, char descipcion[], int duracion);
 
 void insertarNodo(sNodo** cabecera,int id, char descipcion[], int duracion);
 
-void mostrarTarea(sTAREA* t);
+void mostrarTarea(sTAREA t);
 
+sNodo* BuscarTareaID(sNodo * tareasPend, sNodo * tareasReal,int id);
+
+sNodo* BuscarTareaClave(sNodo * tareasPend, sNodo * tareasReal,char clave[]);
 //void mostrarTodasLasTareas(sTAREA** t, int cantidad);
 
-//sTAREA* BuscarTareaID(sTAREA ** tareasPend, sTAREA ** tareasReal,int id, int cantTareas);
-
-//sTAREA* BuscarTareaClave(sTAREA ** tareasPend, sTAREA ** tareasReal,char clave[], int cantTareas);
-
 int main (){
-    char buffer[MAX];
-    int cantTareas,duracion,respuesta,id = 0;;
+    char buffer[MAX],*clave;
+    int cantTareas,duracion,respuesta,id = 0, bandera,idBuscar;
 
-    sNodo* tareasRealizadas; 
-    sNodo* tareaPendientes; 
+    sNodo* tareasRealizadas,* auxMostrar,*auxAnterior; 
+    sNodo* tareaPendientes;
+    sNodo* busqueda; 
     tareasRealizadas = crearListaVacio();
     tareaPendientes = crearListaVacio();
     do
     {
         printf("\n1- Agregar una tarea pendiente: ");
         printf("\n2- Ver y Marcar como realizadas las tareas pendientes: ");
+        printf("\n3- Mostrar Tareas Pendientes y Realizadas");
+        printf("\n4- Buscar por ID");
+        printf("\n5- Buscar por clave");
         printf("\n10- Salir");
         fflush(stdin);
         printf("\nIngrese una opcion: ");
@@ -52,7 +55,6 @@ int main (){
         switch (respuesta)
         {
         case 1 :
-            id ++;
             printf("\nIngrese una descripción para la tarea: ");
             fflush(stdin);
             gets(buffer);
@@ -60,16 +62,105 @@ int main (){
             fflush(stdin);
             scanf("%d",&duracion);
             insertarNodo(&tareaPendientes, id, buffer, duracion);
+            id ++;
             break;
         case 2 :
-            
+            auxMostrar = tareaPendientes;
+            auxAnterior = tareaPendientes;            
+            while (auxMostrar != NULL)
+            {
+                printf("\n==== TAREA: %d ===",auxMostrar->T.TareaID);
+                mostrarTarea(auxMostrar->T);
+                do
+                {
+                    printf("\nRealizo esta tarea? 1 = SI 0 = NO: ");;    
+                    fflush(stdin);
+                    scanf("%d",&respuesta);
+                } while (respuesta != 1 && respuesta != 0);
+                if (respuesta)
+                {
+                    insertarNodo(&tareasRealizadas,auxMostrar->T.TareaID,auxMostrar->T.Descripcion,auxMostrar->T.Duracion);
+                    auxAnterior->Siguiente = auxMostrar->Siguiente;
+                    free(auxMostrar);
+                   
+                }
+                
+                auxAnterior = auxMostrar;
+                auxMostrar = auxMostrar->Siguiente;    
+                
+                
+            }
             break;
+        case 4:
+            printf("\nIngrese el id: ");
+            fflush(stdin);
+            scanf("%d",&idBuscar);
+            busqueda = BuscarTareaID(tareaPendientes,tareasRealizadas,idBuscar);
+            break;
+        case 5:
+            printf("\nIngrese la clave: ");
+            fflush(stdin);
+            gets(buffer);
+            clave = (char *) malloc(sizeof(char) * strlen(buffer) + 1);
+            strcpy(clave,buffer);
+            busqueda = BuscarTareaClave(tareaPendientes,tareasRealizadas,clave);        
+        
         }
-
+        if (respuesta == 4 || respuesta == 5)
+        {
+            if (busqueda != NULL){
+                printf("\n=== LA TAREA BUSCADA ES: ===");
+                mostrarTarea(busqueda->T);
+                printf("\n================");
+            }
+            else
+            {
+                printf("\n===NO HUBO RESULTADOS===");
+            }
+        }
+        
     } while (respuesta != 10);
     
-    
+    free(tareasRealizadas);
+    free(tareaPendientes);
     return 0;
+}
+
+sNodo* BuscarTareaClave(sNodo * tareasPend, sNodo * tareasReal,char clave[]){
+
+    sNodo* aux = tareasPend;
+    while (aux && strstr(aux->T.Descripcion, clave) == NULL)
+    {
+        aux =aux->Siguiente;
+    }
+    if (aux == NULL)
+    {
+        aux = tareasReal;
+        while (aux && strstr(aux->T.Descripcion, clave) == NULL)
+        {
+            aux =aux->Siguiente;
+        }
+    }
+    return aux;
+}
+
+sNodo* BuscarTareaID(sNodo * tareasPend, sNodo * tareasReal,int id){
+
+        sNodo* aux = tareasPend;
+        while (aux != NULL && aux->T.TareaID != id)
+        {
+            aux = aux->Siguiente;
+        }
+        if (aux == NULL)
+        {
+            aux = tareasReal;
+            while (aux!= NULL && aux->T.TareaID != id)
+            {
+                aux = aux->Siguiente;
+            }
+        }
+        return aux;
+
 }
 
 void insertarNodo(sNodo** cabecera,int id, char* descipcion, int duracion){
@@ -90,14 +181,16 @@ sNodo* crearNodo(int id, char descipcion[], int duracion){
 sNodo* crearListaVacio(){
     return NULL;
 }
-void mostrarTarea(sTAREA* t){
 
-    printf("\nID : %d", t->TareaID);
+void mostrarTarea(sTAREA t){
+
+    printf("\nID : %d", t.TareaID);
     printf("\nDescripción: ");
-    puts(t->Descripcion);
-    printf("Duracion: %d",t->Duracion);
+    puts(t.Descripcion);
+    printf("Duracion: %d",t.Duracion);
 
 }
+
 
 
 
